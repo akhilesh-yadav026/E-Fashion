7c63e9662ef833fb05cd889872cb77823215ed65
akhilesh-yadav026 <akhilesh9426@gmail.com>
2025-06-27 12:12:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import AboutUs from './pages/AboutUs'
+import Contact from './pages/Contact'
+import Collection from './pages/Collection'
+import PlaceOrder from './pages/PlaceOrder'
+import Login from './pages/Login'
+import Product from './pages/Product'
+import Orders from './pages/Orders'
+import Cart from './pages/Cart'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<AboutUs />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/collection' element={<Collection />} />
+        <Route path='/place-order' element={<PlaceOrder />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/product' element={<Product />} />
+        <Route path='/orders' element={<Orders />} />
+        <Route path='/cart' element={<Cart/>} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
