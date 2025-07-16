78ee54415d49e668977f8b854dc34695aa89224a
akhilesh-yadav026 <akhilesh9426@gmail.com>
2025-07-16 11:25:08 +0530

    Add and Update

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import Title from '../components/Title'
+import {assets} from '../assets/assets'
+import NewsletterBox from '../components/NewsletterBox'
 
 const AboutUs = () => {
   return (
-    <div>AboutUs</div>
+    <div>
+
+    <div className='text-2xl text-center pt-8 border-t'>
+        <Title text1={'ABOUT'} text2={'US'} />
+    </div>
+
+    <div className="my-10 flex flex-col md:flex-row gap-16">
+      <img className='w-full md:max-w-[450px] ' src={assets.about_img} alt="" />
+      <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia tempore nulla ullam dolores fugiat itaque.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem quidem itaque unde tempore voluptatibus laborum?</p>
+      <b className='text-gray-800'>Our Mission</b>
+      <p>Our mission at abc is to empower customers with choice , convenience andd confidence. We're decide to provideing a seamless shopping experience that exceeds expectations,from browsing and ordering to delivery and beyond. </p>
+      
+      </div>
+    </div>
+    <div className='text-xl py-4'>
+      <Title text1={'WHY'} text2={'CHOOSE US'} />
+    </div>
+    <div className="flex flex-col md:flex-row text-sm mb-20">
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Quality Assurance:</b>
+          <p className='text-gray-600'>We meticulously select and vet each product to ensure it meets our stringent quality assurance </p>
+        </div>
+           <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Convenience:</b>
+          <p className='text-gray-600'>With our user-friendly interface and hassle-free orderinng process, shoopping has never been. </p>
+        </div>
+            <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Exceptional Customer Service :</b>
+          <p className='text-gray-600'> Our team of dedication professionals is here to assist you the way, ensuring your satisfaction is our top priority. </p>
+        </div>
+    </div>
+    <NewsletterBox />
+    </div>
   )
 }
 
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
+import NewsletterBox from '../components/NewsletterBox'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      <div className="text-center text-2xl pt-10 border-t">
+        <Title text1={'CONTACT'} text2={'US'} />
+      </div>
+      <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28">
+        <img src={assets.contact_img} alt="" className="w-full md:max-w-[480px]" />
+        <div className="flex flex-col justify-center items-start gap-6">
+          <p className='font-semibold text-xl text-grayy-600'>Our Store</p>
+          <p className='text-gray-500'>123 Kalupur Station, <br />Near Sargpurpur AMTS bus terminal.</p>
+          <p className='text-gray-500'>Tel: (415) 555-01 <br /> Email: admin@abc.com</p>
+          <p className='font-semibold text-xl text-gray-600'>Careers at Abc</p>
+          <p className='text-gray-500'>Learn more about our teams and job openings.</p>
+          <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
+        </div>
+      </div>
+      <NewsletterBox />
+    </div>
   )
 }
 
